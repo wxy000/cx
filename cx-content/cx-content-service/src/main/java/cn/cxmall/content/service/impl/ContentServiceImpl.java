9b9b8581eb4dfeb8f74d24c5273c1f3538cacf31
wxy000 <396310583@qq.com>
2018-08-24 11:07:35 +0800

    前台增加redis缓存，可减轻数据库压力

--- cx-content/cx-content-service/src/main/java/cn/cxmall/content/service/impl/ContentServiceImpl.java
+++ cx-content/cx-content-service/src/main/java/cn/cxmall/content/service/impl/ContentServiceImpl.java
@@ -1,14 +1,18 @@
 package cn.cxmall.content.service.impl;
 
+import cn.cxmall.common.jedis.JedisClient;
 import cn.cxmall.common.result.CxResult;
 import cn.cxmall.common.result.DataGridResult;
+import cn.cxmall.common.utils.JsonUtils;
 import cn.cxmall.content.service.ContentService;
 import cn.cxmall.mapper.TbContentMapper;
 import cn.cxmall.pojo.TbContent;
 import cn.cxmall.pojo.TbContentExample;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -23,6 +27,11 @@ public class ContentServiceImpl implements ContentService {
 
     @Autowired
     private TbContentMapper contentMapper;
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Value("${CONTENT_LIST}")
+    private String CONTENT_LIST;
 
     /**
      * 根据categoryid查询内容
@@ -60,6 +69,8 @@ public class ContentServiceImpl implements ContentService {
         tbContent.setCreated(new Date());
         tbContent.setUpdated(new Date());
         contentMapper.insert(tbContent);
+        //缓存同步
+        jedisClient.hdel(CONTENT_LIST, tbContent.getCategoryId().toString());
         return CxResult.ok();
     }
 
@@ -73,6 +84,8 @@ public class ContentServiceImpl implements ContentService {
         //补全信息
         tbContent.setUpdated(new Date());
         contentMapper.updateByPrimaryKeySelective(tbContent);
+        //缓存同步
+        jedisClient.hdel(CONTENT_LIST, tbContent.getCategoryId().toString());
         return CxResult.ok();
     }
 
@@ -83,6 +96,9 @@ public class ContentServiceImpl implements ContentService {
      */
     @Override
     public CxResult deleteContentById(long id) {
+        //缓存同步
+        TbContent tbContent = contentMapper.selectByPrimaryKey(id);
+        jedisClient.hdel(CONTENT_LIST, tbContent.getCategoryId().toString());
         contentMapper.deleteByPrimaryKey(id);
         return CxResult.ok();
     }
@@ -107,12 +123,29 @@ public class ContentServiceImpl implements ContentService {
      */
     @Override
     public List<TbContent> getContentList(long categoryId) {
+        //从redis缓存中取数据
+        try {
+            String hget = jedisClient.hget(CONTENT_LIST, categoryId + "");
+            if (StringUtils.isNotBlank(hget)){
+                List<TbContent> contents = JsonUtils.jsonToList(hget, TbContent.class);
+                return contents;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //如果redis中没有则到数据库中查
         //创建example
         TbContentExample example = new TbContentExample();
         //设置查询条件
         TbContentExample.Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(categoryId);
         List<TbContent> list = contentMapper.selectByExample(example);
+        //将查询结果保存到redis中
+        try {
+            jedisClient.hset(CONTENT_LIST, categoryId + "", JsonUtils.objectToJson(list));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return list;
     }
 }
